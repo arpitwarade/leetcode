962aec21a3cf3d8c78454aafb4b8e6a8c729dd37
Arpit Warade <153924502+arpitwarade@users.noreply.github.com>
2026-04-11 23:32:43 +0530

    Time: 1538 ms (0.87%) | Memory: 42.8 MB (9.37%) - LeetSync

---
+++ 231-power-of-two/power-of-two.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        // int max = Integer.MAX_VALUE;
+        int count =1;
+        int max = n/2;
+        if( n<=0){
+            return false;
+        }
+        if(n ==1){
+            return true;
+        }
+        for(int i=0; i<max; i++){
+                count *=2;
+            if(n == count){
+                return true;
+            }
+            else if(count>n){
+                return false;
+            }
+
+        }
+        return false;
+        
+    }
+}
