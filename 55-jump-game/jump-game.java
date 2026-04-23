ce7899b23c2b3cd75fcc2a54651ddbc73eaa7353
Arpit Warade <153924502+arpitwarade@users.noreply.github.com>
2026-04-23 23:36:11 +0530

    Time: 2 ms (86.26%) | Memory: 48.2 MB (10.23%) - LeetSync

---
+++ 55-jump-game/jump-game.java
@@ -0,0 +1,29 @@
+// class Solution {
+//     public boolean canJump(int[] nums) {
+//         if(nums.length == 1){
+//             return true;
+//         }
+//         for(int i =0; i<nums.length; i++){
+//             if((nums[i]+i) == nums.length -1){
+//                 return true;
+//             }
+//         }
+//         return false;       
+//     }
+// }
+class Solution{
+     
+    public boolean canJump(int[] nums){
+        if(nums.length == 1){
+        return true;
+      }
+        int maxReach =0;
+        for(int i =0; i<nums.length; i++){
+            if(i>maxReach){
+                return false;
+            }
+            maxReach = (int)Math.max(maxReach,(nums[i]+i));
+        }
+        return maxReach>=nums.length-1;
+    }
+}
