6d052fec5bd9392d18cef162bbd8270a760e0e3b
Arpit Warade <153924502+arpitwarade@users.noreply.github.com>
2026-04-27 00:30:36 +0530

    Time: 20 ms (51.18%) | Memory: 47.2 MB (62.74%) - LeetSync

--- 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
+++ 1371-minimum-remove-to-make-valid-parentheses/minimum-remove-to-make-valid-parentheses.java
@@ -17,6 +17,7 @@ class Solution {
             sb.append(s.charAt(i));
          }
         }
+    
         StringBuilder newsb = new StringBuilder();
         for(int i = sb.length()-1; i>=0; i--){
             if(sb.charAt(i) == '(' && cnt>0){
