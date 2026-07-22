5b22da061117209d662a56f57f78c99d0c25b428
Arpit Warade <153924502+arpitwarade@users.noreply.github.com>
2026-07-22 19:55:52 +0530

    Time: 18 ms (41.15%) | Memory: 45.8 MB (13.99%) - LeetSync

--- 16-3sum-closest/3sum-closest.java
+++ 16-3sum-closest/3sum-closest.java
@@ -1,27 +1,32 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
-           int var =0;
-           int cons =0;
-           int s = 0;
-           int e = Integer.MAX_VALUE;
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                for (int k = j + 1; k < nums.length; k++) {
-                      
-                       var = nums[i]+nums[j]+nums[k];
-                     if(var == target){
-                        return var;
-                     }
-                     else{
-                        cons = (int)Math.abs(var-target);
-                        if(cons < e){
-                            e = cons;
-                            s = var;
-                        }
-                     } 
+        Arrays.sort(nums);
+
+        int nearSum = nums[0] + nums[1] + nums[2];
+
+        for (int fIdx = 0; fIdx < nums.length - 2; fIdx++) {
+
+            int sIdx = fIdx + 1;
+            int lIdx = nums.length - 1;
+
+            while (sIdx < lIdx) {
+
+                int add = nums[fIdx] + nums[sIdx] + nums[lIdx];
+
+                if (Math.abs(add - target) < Math.abs(nearSum - target)) {
+                    nearSum = add;
+                }
+
+                if (add == target) {
+                    return add;
+                } else if (add < target) {
+                    sIdx++;
+                } else {
+                    lIdx--;
                 }
             }
         }
-        return s;
+
+        return nearSum;
     }
 }
