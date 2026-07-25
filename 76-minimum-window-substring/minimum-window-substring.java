1c7a01c142bb22ad3b9c6c8a9d222e81ce6503e2
Arpit Warade <153924502+arpitwarade@users.noreply.github.com>
2026-07-25 16:06:46 +0530

    Time: 2 ms (99.77%) | Memory: 46.3 MB (61.60%) - LeetSync

---
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -0,0 +1,44 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        int []freqS = new int[256];
+        int []freqT = new int[256];
+         
+
+         int n = s.length();
+         int m = t.length();
+         int low = 0;
+         int match = 0;
+         int start =0;
+         int res = Integer.MAX_VALUE;
+
+         if(m>n) return"";
+         for(int i = 0; i<m; i++){
+            freqT[t.charAt(i)]++;
+         }
+         for(int high = 0; high<n; high++){
+            int ch =s.charAt(high);
+            freqS[ch]++;
+
+            if(freqS[ch]<=freqT[ch]){
+                match++;
+            }
+
+            while(match == m){
+                if(high-low+1<res){
+                    res = high-low+1;
+                    start = low;
+                }
+               char left = s.charAt(low);
+                freqS[left]--;
+
+                if (freqS[left] < freqT[left]) {
+                    match--;
+                }
+
+                low++;
+            }
+
+         }
+            return res == Integer.MAX_VALUE ? "" : s.substring(start, start + res);
+    }
+}
