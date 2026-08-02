7b6fb774e977b2407b6716cf2ac87a6be495ef9e
Arpit Warade <153924502+arpitwarade@users.noreply.github.com>
2026-08-02 12:38:55 +0530

    Time: 3 ms (13.96%) | Memory: 47.1 MB (57.29%) - LeetSync

---
+++ 57-insert-interval/insert-interval.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        intervals = Arrays.copyOf(intervals, intervals.length + 1);
+        intervals[intervals.length - 1] = newInterval;
+        List<int[]> list = new ArrayList<>();
+            Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+            int start = intervals[0][0];
+            int end  = intervals[0][1];
+            int n = intervals.length;
+                for(int i =1; i<n; i++){
+                int nextStart = intervals[i][0];
+                int nextEnd = intervals[i][1];
+
+                if(nextStart <= end){
+                    end = Math.max(end, nextEnd);
+                }
+                else{
+                    list.add(new int[]{start, end});
+                    start = nextStart;
+                    end = nextEnd;
+                }
+                }
+                list.add(new int[]{start,end});
+                return list.toArray(new int[list.size()][]);
+            }
+        
+    }
