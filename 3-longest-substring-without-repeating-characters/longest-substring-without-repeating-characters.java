11fc4e63eba8eb86549ccdf6f84a0c14b9e1f438
Arpit Warade <153924502+arpitwarade@users.noreply.github.com>
2026-08-13 10:31:28 +0530

    Time: 75 ms (9.47%) | Memory: 47.5 MB (24.29%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,26 +1,26 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int n = s.length();
-        int low =0;
-        int res = 0;
-        Map<Character, Integer> map = new HashMap<>();
+//         int n = s.length();
+//         int low =0;
+//         int res = 0;
+//         Map<Character, Integer> map = new HashMap<>();
 
-        for(int high =0; high<n; high++){
-            char ch = s.charAt(high);
-            map.put(ch,map.getOrDefault(ch,0)+1);
-            int k = high-low+1;
+//         for(int high =0; high<n; high++){
+//             char ch = s.charAt(high);
+//             map.put(ch,map.getOrDefault(ch,0)+1);
+//             int k = high-low+1;
 
-            while(map.size()< k){
-                char leftchar = s.charAt(low);
-                map.put(leftchar, map.get(leftchar) - 1);
-                if (map.get(leftchar) == 0)
-                    map.remove(leftchar);
-                low++;
-                k = high-low+1;
-                    }
-                   res =Math.max(res,k); 
-          }
-return res;
+//             while(map.size()< k){
+//                 char leftchar = s.charAt(low);
+//                 map.put(leftchar, map.get(leftchar) - 1);
+//                 if (map.get(leftchar) == 0)
+//                     map.remove(leftchar);
+//                 low++;
+//                 k = high-low+1;
+//                     }
+//                    res =Math.max(res,k); 
+//           }
+// return res;
         // Set<Character>set=new HashSet<>();
         // int maxLength=0;
         // int left=0;
@@ -41,6 +41,26 @@ return res;
             
         // }
         // return maxLength;
+        int low = 0;
+        int res = 0;
+        Map<Character,Integer> mp = new HashMap<>();
+        for(int high = 0; high <s.length(); high++){
+            char ch = s.charAt(high);
+            mp.put(ch,mp.getOrDefault(ch,0)+1);
+            int k = high - low +1;
+
+            while(mp.size() <k){
+                char c = s.charAt(low);
+                mp.put(c,mp.getOrDefault(c, 0)-1);
+                low++;
+                if(mp.get(c) == 0){
+                    mp.remove(c);
+                }
+                k = high-low+1;
+            }
+            res = Math.max(res,k); 
+        }
+        return res;
 
 
         
