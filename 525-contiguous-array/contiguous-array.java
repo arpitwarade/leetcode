8af17e116b4ca98c3e30ee0eeb332178f3dc6d87
Arpit Warade <153924502+arpitwarade@users.noreply.github.com>
2026-08-14 17:18:05 +0530

    Time: 25 ms (19.84%) | Memory: 65.5 MB (60.23%) - LeetSync

--- 525-contiguous-array/contiguous-array.java
+++ 525-contiguous-array/contiguous-array.java
@@ -1,31 +1,59 @@
 class Solution {
     public int findMaxLength(int[] nums) {
-        int n = nums.length;
-        int zero = 0;
-        int one = 0;
-        Map<Integer,Integer> map = new HashMap<>();
+    //     int n = nums.length;
+    //     int zero = 0;
+    //     int one = 0;
+    //     Map<Integer,Integer> map = new HashMap<>();
 
-        int res =0;
-        for(int i =0; i<n; i++){
-            if(nums[i] == 0){
-                zero++;
+    //     int res =0;
+    //     for(int i =0; i<n; i++){
+    //         if(nums[i] == 0){
+    //             zero++;
+    //         }
+    //         else{
+    //             one++;
+    //         }
+    //         int diff = zero-one;
+    //         if(diff ==0){
+    //             res= Math.max(res,i+1);
+    //         }
+    //         if(!map.containsKey(diff)){
+    //             map.put(diff,i);
+    //         }
+    //         else{
+    //             int idx = map.get(diff);
+    //             int len = i-idx;
+    //             res = Math.max(len,res);
+    //         }
+    //     }
+    //     return res;
+    // }
+            int n =nums.length;
+            int zero = 0;
+            int one = 0;
+            Map<Integer,Integer>mp= new HashMap<>();
+            int res =0;
+            for(int i =0; i< n; i++){
+                if(nums[i] == 0){
+                    zero++;
+                }
+                else{
+                    one++;
+                }
+                int diff = zero - one;
+
+                if(diff == 0){
+                    res = Math.max(res, i+1);
+                }
+                if(!mp.containsKey(diff)){
+                    mp.put(diff, i);
+                }
+                else{
+                    int z = mp.get(diff);
+                    int len = i-z;
+                    res = Math.max(res, len);
+                }
             }
-            else{
-                one++;
-            }
-            int diff = zero-one;
-            if(diff ==0){
-                res= Math.max(res,i+1);
-            }
-            if(!map.containsKey(diff)){
-                map.put(diff,i);
-            }
-            else{
-                int idx = map.get(diff);
-                int len = i-idx;
-                res = Math.max(len,res);
-            }
-        }
-        return res;
+            return res;
     }
 }
