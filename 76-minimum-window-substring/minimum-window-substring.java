176946624609f827da97cf5bfb47edef2b362fa1
Arpit Warade <153924502+arpitwarade@users.noreply.github.com>
2026-08-16 12:38:58 +0530

    Time: 6 ms (61.26%) | Memory: 46 MB (63.20%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.java
+++ 76-minimum-window-substring/minimum-window-substring.java
@@ -2,43 +2,41 @@ class Solution {
     public String minWindow(String s, String t) {
         int []freqS = new int[256];
         int []freqT = new int[256];
-         
 
-         int n = s.length();
-         int m = t.length();
-         int low = 0;
-         int match = 0;
-         int start =0;
-         int res = Integer.MAX_VALUE;
+        int match =0;
+        int start = 0;
+        int low =0;
+        int res = Integer.MAX_VALUE;
+        int n = s.length();
+        int m = t.length();
+        if(n < m){
+            return "";
+        } 
 
-         if(m>n) return"";
-         for(int i = 0; i<m; i++){
+        for(int i =0; i<m; i++){
             freqT[t.charAt(i)]++;
-         }
-         for(int high = 0; high<n; high++){
-            int ch =s.charAt(high);
-            freqS[ch]++;
+        }
 
-            if(freqS[ch]<=freqT[ch]){
+        for(int j =0; j<n; j++){
+            freqS[s.charAt(j)]++;
+
+            if(freqS[s.charAt(j)] <= freqT[s.charAt(j)]){
                 match++;
             }
 
             while(match == m){
-                if(high-low+1<res){
-                    res = high-low+1;
-                    start = low;
+                if(j-low+1<res){
+                    res = j-low+1;
+                    start =low;
                 }
-               char left = s.charAt(low);
+                char left = s.charAt(low);
                 freqS[left]--;
-
-                if (freqS[left] < freqT[left]) {
+                if(freqS[left] < freqT[left]){
                     match--;
                 }
-
                 low++;
+            }            
+        }
+        return res == Integer.MAX_VALUE ? "" : s.substring(start, start+res);
             }
-
-         }
-           return res == Integer.MAX_VALUE ? "" : s.substring(start, start + res);
-    }
 }
