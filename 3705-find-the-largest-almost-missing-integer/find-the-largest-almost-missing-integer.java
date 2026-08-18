2d43235d860b3a9a921bb794f8f33ebd91383ae3
Arpit Warade <153924502+arpitwarade@users.noreply.github.com>
2026-08-18 11:07:19 +0530

    Time: 3 ms (63.51%) | Memory: 44.7 MB (71.93%) - LeetSync

--- 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.java
+++ 3705-find-the-largest-almost-missing-integer/find-the-largest-almost-missing-integer.java
@@ -1,36 +1,62 @@
 class Solution {
     public int largestInteger(int[] nums, int k) {
-        Map<Integer,Integer> freq = new HashMap<>();
-        Map<Integer,Integer> window = new HashMap<>();
+        // Map<Integer,Integer> freq = new HashMap<>();
+        // Map<Integer,Integer> window = new HashMap<>();
 
-        for(int i = 0; i<k; i++){
-            freq.put(nums[i],freq.getOrDefault(nums[i],0)+1);
-        }
-        for(int i: freq.keySet()){
-            window.put(i, 1);
-        }
+        // for(int i = 0; i<k; i++){
+        //     freq.put(nums[i],freq.getOrDefault(nums[i],0)+1);
+        // }
+        // for(int i: freq.keySet()){
+        //     window.put(i, 1);
+        // }
 
-        for(int i = k; i<nums.length; i++){
+        // for(int i = k; i<nums.length; i++){
 
-            int out = nums[i-k];
-            freq.put(out, freq.getOrDefault(out,0)-1);
-            if(freq.get(out)==0){
-                freq.remove(out);
+        //     int out = nums[i-k];
+        //     freq.put(out, freq.getOrDefault(out,0)-1);
+        //     if(freq.get(out)==0){
+        //         freq.remove(out);
+        //     }
+
+        //     int in = nums[i];
+        //     freq.put(in, freq.getOrDefault(in, 0)+1);
+
+        //     for(int j : freq.keySet()){
+        //         window.put(j, window.getOrDefault(j,0)+1);
+        //     }
+        // }
+        // int ans = -1;
+
+        // for(int i: window.keySet()){
+        //     if(window.get(i) == 1){
+        //         ans= Math.max(ans,i);
+        //     }
+        // }
+        // return ans;
+           if (k == nums.length) {
+            Set<Integer> set = new HashSet<>();
+
+            int ans = -1;
+
+            for (int x : nums) {
+                if (set.add(x)) {
+                    ans = Math.max(ans, x);
+                }
             }
 
-            int in = nums[i];
-            freq.put(in, freq.getOrDefault(in, 0)+1);
-
-            for(int j : freq.keySet()){
-                window.put(j, window.getOrDefault(j,0)+1);
-            }
+            return ans;
         }
-        int ans = -1;
-
-        for(int i: window.keySet()){
-            if(window.get(i) == 1){
-                ans= Math.max(ans,i);
+        Map<Integer,Integer> mp = new HashMap<>();
+        for(int i =0; i<nums.length; i++){
+            int window = Math.min(i+1, k) -Math.max(0,i-(nums.length-k));
+            mp.put(nums[i], mp.getOrDefault(nums[i],0)+window);
+        }
+        int ans =-1;
+        for(int i: mp.keySet()){
+            if(mp.get(i) == 1){
+                ans = Math.max(i,ans);
             }
+
         }
         return ans;
     }
