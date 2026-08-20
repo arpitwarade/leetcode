256acb2be23ced43a5bf4b37a5ba8e53e665dc1e
Arpit Warade <153924502+arpitwarade@users.noreply.github.com>
2026-08-20 12:12:05 +0530

    Time: 3 ms (67.52%) | Memory: 44.1 MB (15.38%) - LeetSync

---
+++ 1666-make-the-string-great/make-the-string-great.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String makeGood(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for(int i = 0; i< s.length(); i++){
+            if(!st.empty() && Math.abs(st.peek()-s.charAt(i)) == 32){
+                st.pop();
+            }
+            else{
+                st.push(s.charAt(i));
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+
+        while(!st.empty()){
+            sb.append(st.peek());
+            st.pop();
+        }
+        return sb.reverse().toString();
+    }
+}
