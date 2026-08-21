1c9e750a8e1357a0407ddb339afe5ef1cbc3bbc8
Arpit Warade <153924502+arpitwarade@users.noreply.github.com>
2026-08-22 00:04:25 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (10.56%) - LeetSync

--- 777-toeplitz-matrix/toeplitz-matrix.java
+++ 777-toeplitz-matrix/toeplitz-matrix.java
@@ -1,14 +1,25 @@
 class Solution {
     public boolean isToeplitzMatrix(int[][] matrix) {
-        int rows = matrix.length;
-        int cols = matrix[0].length;
-        for(int i=1; i<rows; i++){
-            for(int j=1; j<cols; j++){
-                if(matrix[i][j] != matrix[i-1][j-1]){
+        // int rows = matrix.length;
+        // int cols = matrix[0].length;
+        // for(int i=1; i<rows; i++){
+        //     for(int j=1; j<cols; j++){
+        //         if(matrix[i][j] != matrix[i-1][j-1]){
+        //             return false;
+        //         }
+        //     }
+        // }  
+        // return true;
+
+        int i = 0;
+        while(i < matrix.length-1){
+            for(int j = 0; j<matrix[0].length-1; j++){
+                if(matrix[i][j] != matrix[i+1][j+1]){
                     return false;
                 }
             }
-        }  
-        return true;     
+            i++;
+        }     
+        return true;
     }
 }
